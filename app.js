ce871b1b8d0cccd871e7820ca1dbd548cf2d15f2
thetaha1 <millionertaha@gmail.com>
2023-09-11 20:46:47 +0300

    during

---
+++ app.js
@@ -0,0 +1,10 @@
+let form = document.getElementById("form-section");
+let nameInput = document.getElementById("name");
+let emaiInput = document.getElementById("emai");
+let phoneInput = document.getElementById("phone");
+
+form.addEventListener("submit", function(event) {
+    let isValid = true;
+
+    let nameRegix
+})
